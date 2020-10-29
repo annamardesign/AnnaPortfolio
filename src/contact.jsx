a4621ed770d276ea44a928ee882e4a0d6794d500
Anna <lalogica@gmail.com>
2020-10-29 19:42:16 +0200

    Contact Form CSS

--- src/contact.jsx
+++ src/contact.jsx
@@ -30,25 +30,31 @@ class Contact extends Component {
     render() { 
         return (
         <div className="page">
-         <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+        <section className="main-section">
+            <div className="contact"><p className="contact-text">Contact me</p></div>
+         <form className="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+          <div className="input-group">
            <label htmlFor="name">Name:</label>
            <input type="text"
             className="form-input" 
             value={this.state.name}
             onChange={this.onNameChange.bind(this)}/>
-
-           <label htmlFor="number">Phone Number:</label>
+         </div><br />
+         <div className="input-group">
+           <label htmlFor="number">Phone:</label>
            <input type="number" 
            className="form-input" 
            value={this.state.number} 
            onChange={this.onNumberChange.bind(this)}/>
-
+         </div><br />
+         <div className="input-group">
            <label htmlFor="email">Email:</label>
            <input type="email" 
            className="form-input" 
            value={this.state.email} 
            onChange={this.onEmailChange.bind(this)}/>
-
+         </div><br />
+         <div className="input-group">
            <label htmlFor="message">Message</label>
            <textarea 
            className="form-control" 
@@ -57,7 +63,9 @@ class Contact extends Component {
            onChange={this.onMessageChange.bind(this)} />
           
           <button type="submit" className="send-btn">Send</button>
+          </div><br />
         </form>
+        </section>
     </div>  
            );
     }
