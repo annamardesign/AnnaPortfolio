8f95adaab962ff41bb5d47a354bb43b67588afb3
Anna <lalogica@gmail.com>
2020-11-03 16:15:07 +0200

    FingerPrint Panel

--- src/common/drawing.jsx
+++ src/common/drawing.jsx
@@ -1,31 +1,38 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { GiFingerPrint } from "react-icons/gi";
 class DrawingPanel extends Component {
     constructor(props){
         super(props)
         this.state = {
-          x: [],
-          y: []
+          x: 0,
+          y: 0,
+          canvasIsClicked: false,
+          numPrints:0
         }
     }
     handleMouseMove = (e) => {
         this.setState({ x: e.screenX, y: e.screenY });
     }
-    
-    draw(props) {
-            
-        if(this.handleMouseMove === true) {
-         return <span/>
-        }
+    handleAddFingerPrint = (e) => {
+        this.setState({ canvasIsClicked: true })
+    }
+    onAddFingerPrint = (e) => {
+      this.setState({
+        numPrints: this.state.numPrints + 1
+      });
     }
         
-        
           render() {
-            const { x, y } = this.state;
-            return <div className="canvas" 
-            style={{width:"300px", height:"300px", backgroundColor:"grey"}}
-            onMouseMove={this.handleMouseMove}>
-              <span style={{x: this.state.x, y: this.state.y, width: "15px", height:"0px", backgroundColor:"red", zIndex:"1"}}><p>Draw here</p></span>
-            </div>;
+            const { x, y, image } = this.state;
+            const children = [];
+            for (let i = 0; i < this.state.numPrints; i += 1) {
+              children.push(<GiFingerPrint key={i} number={i} />);
+            };
+            return <div className="canvas" onClick={this.onAddFingerPrint}
+            style={{width:"500px", height:"500px", borderRadius:"50%", backgroundColor:"#222222"}}
+            onMouseMove={this.handleMouseMove} > {children}
+              <p>Leave your print</p>
+            </div>
           }
         }
 
