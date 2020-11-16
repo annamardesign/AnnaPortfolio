f84605dd6a233743259eec350faec8f46c995304
Anna Marinova <lalogica@gmail.com>
2020-11-16 18:02:54 +0200

    Logo resized and refactored

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -4,21 +4,22 @@ import { FiTwitter} from 'react-icons/fi';
 import { FiGithub } from 'react-icons/fi';
 import { RiBehanceLine} from 'react-icons/ri';
 import { FiCodepen } from 'react-icons/fi';
+import Logo from './logo.jsx';
 import "./navbar.css";
 
 const NavBar = () => {
   
     return (<div className="topnav">
-    <NavLink to="/resume" className="logo" >Logo</NavLink>
+    <NavLink to="/resume" className="logo" ><Logo /></NavLink>
     <NavLink to="/resume" className="navli" activeClassName="active">Resume</NavLink>
     <NavLink to="/projects" className="navli" activeClassName="active">Projects</NavLink>
     <NavLink to="/freebies" className="navli" activeClassName="active">Freebies</NavLink>
     <NavLink to="/contact" className="navli" activeClassName="active">Contact</NavLink>
     <ul className="social-container">
-    <li className="icon"><a href="https://www.twitter.com/annamardesign" className="social" activeClassName="active"><FiTwitter /></a></li>
-    <li className="icon"><a href="https://www.github.com/annamardesign" className="social" activeClassName="active"><FiGithub /></a></li>
-    <li className="icon"><a href="https://www.behance.net/AnnaMarino" className="social" activeClassName="active"><RiBehanceLine /></a></li>
-    <li className="icon"><a href="https://www.codepen.io/annamardesigns/pens/" className="social" activeClassName="active"><FiCodepen /></a></li>
+    <li className="icon"><a href="https://www.twitter.com/annamardesign" className="social" activeclassname="active"><FiTwitter /></a></li>
+    <li className="icon"><a href="https://www.github.com/annamardesign" className="social" activeclassname="active"><FiGithub /></a></li>
+    <li className="icon"><a href="https://www.behance.net/AnnaMarino" className="social" activeclassname="active"><RiBehanceLine /></a></li>
+    <li className="icon"><a href="https://www.codepen.io/annamardesigns/pens/" className="social" activeclassname="active"><FiCodepen /></a></li>
     </ul>
   </div>  );
 }
--- src/resume.jsx
+++ src/resume.jsx
@@ -13,7 +13,7 @@ export default class Resume extends Component {
               < FingerPrintPanel className="fingerprint-panel" />
               < DrawingPanel className="drawing-panel" />
               </main>
-              <sidebar className="about-me-center">
+              <section className="about-me-center">
               
                   <img src={ require("./images/Anna.png")} style={{width:"50%"}}/>
                   <div className="text">
@@ -21,7 +21,7 @@ export default class Resume extends Component {
                   <p> I have Professional Degree in Web Design from NET IT Ltd. Web Academy. My superpowers are <span style={{color:"#ff3e55"}}>HTML5, CSS, JavaScript, React</span>.</p>
                   <p> I love to draw digitally and make mosaics on the side. </p>
                   </div>
-              </sidebar>
+              </section>
               
             </div> </React.Fragment>
         )
